ed0afccbcfe2e7b5214ef1ce517a0a3dc4fbbc05
Faisal L <salluthdev@gmail.com>
2021-08-28 17:42:19 +0700

    Pembersihan Kode JavaScript

--- assets/script.js
+++ assets/script.js
@@ -2,19 +2,24 @@ $(document).ready(function()
 {
 	$('.click').click(function()
 	{
-		$('.popup_box').css({
+		$('.popup_box').css(
+		{
 			"opacity":"1", "pointer-events":"auto"
 		});
 	});
-	$('.btn_no').click(function(){
-		$('.popup_box').css({
+	$('.btn_no').click(function()
+	{
+		$('.popup_box').css(
+		{
 			"opacity":"0", "pointer-events":"none"
 		});
 	});
-	$('.btn_yes').click(function(){
-		$('.popup_box').css({
+	$('.btn_yes').click(function()
+	{
+		$('.popup_box').css(
+		{
 			"opacity":"0", "pointer-events":"none"
 		});
-		alert("Your Content Deleted Succesfully.")
+		alert("Your Content Deleted Succesfully.");
 	});
 });
